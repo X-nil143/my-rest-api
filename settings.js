66a8e9d3cb232d4b6f526554b8abd5faad6a2906
X-nil143 <140935083+X-nil143@users.noreply.github.com>
2024-02-01 06:50:39 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://xnil:xnil143@cluster0.kpqoc6s.mongodb.net/" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "jubayermahin7@gmail.com" //email
+global.email_password = "jvbcekthaoelszhh" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
